166016c336cdc417eeacdc3b31880af6f7d2a698
Narciso Jaramillo <njaramil@adobe.com>
2012-05-25 20:17:34 -0700

    Made build script less sensitive to where you run it from

--- build/brackets.build.js
+++
@@ -1,9 +0,0 @@
-({
-    baseUrl: "../src/",
-    optimize: "none",
-    optimizeCss: "none",
-    useStrict: true,
-    name: "brackets",
-    include: ["thirdparty/require"],
-    out: "../dist/tmp/brackets.concat.js"
-})
